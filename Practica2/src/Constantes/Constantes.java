f9b2ef8b95894f48f11cbb3808de69ba87669a8b
usuario_local <usuario_local@pto0407>
2014-11-27 19:12:37 +0100

    añadidas constantes

--- Practica2/src/Constantes/Constantes.java
+++ Practica2/src/Constantes/Constantes.java
@@ -2,10 +2,29 @@ package Constantes;
 
 public final class Constantes {
 	public static String TIEMPO_EXT = "tiempo exterior";
+	
+	public static String SOLEADO = "soleado";
+	public static String NUBLADO = "nublado";
+	public static String LLUVIOSO = "lluvioso";
+	
 	public static String TEMPERATURA = "temperatura";
+		
+	public static String CALUROSO = "caluroso";
+	public static String FRIO = "frio";
+	public static String TEMPLADO = "templado";
+	
 	public static String HUMEDAD = "humedad";
+	
+	public static String ALTA = "alta";
+	public static String NORMAL = "normal";
+	
 	public static String VIENTO = "viento";
+	
+	public static String FALSO = "falso";
+	public static String VERDAD = "verdad";
+	
 	public static String JUGAR = "jugar";
-
-	public static String JUGAR = "jugar";
+	
+	public static String NO = "no";
+	public static String SI = "si";
 }
